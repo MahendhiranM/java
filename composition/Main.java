792a75d5513628ca63900c33fb2c2408bd08eaf5
Mahendhiran M <60248720+MahendhiranM@users.noreply.github.com>
2022-09-22 21:46:20 +0530

    delete composition directory

--- composition/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-
-	public static void main(String[] args){
-		Person p1 = new Person("Mahendhiran", new DateOfBirth(25, 6, 2000), "Salem");
-		System.out.println(p1.toString());
-	}
-}
